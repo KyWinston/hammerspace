473eada21b66059d89f1d0b77fea08fe6ad57b88
KyleWinston <kyle.w.r@hotmail.com>
2024-03-22 14:07:15 -0400

    prepping for crates.io

---
+++ src/viewer/components.rs
@@ -0,0 +1,22 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+pub struct PanOrbitCamera {
+    pub orbit: f32,
+    pub focus: Vec3,
+    pub radius: f32,
+    pub upside_down: bool,
+    pub panning: bool,
+}
+
+impl Default for PanOrbitCamera {
+    fn default() -> Self {
+        PanOrbitCamera {
+            orbit: 0.0,
+            focus: Vec3::ZERO,
+            radius: 5.0,
+            upside_down: false,
+            panning: false,
+        }
+    }
+}
