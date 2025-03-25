551e561795642da988eb314e1602bdb44e7f4e64
ky roberts <kyle.w.r@hotmail.com>
2025-03-25 16:50:53 -0400

    migration work

--- src/assembler/resources.rs
+++ src/assembler/resources.rs
@@ -1,6 +1,6 @@
 // use super::{events::PostProgresssEvent, AssetLoadState};
 use bevy::{asset::Handle, gltf::Gltf, platform_support::collections::HashMap, prelude::*};
-#[cfg(feature="load_progress")]
+#[cfg(feature = "load_progress")]
 use iyes_progress::ProgressEntry;
 
 #[derive(Resource)]
@@ -100,7 +100,7 @@ pub(crate) fn init_resources(
     commands.insert_resource(MeshAssetsLoading(loading_meshes.clone()));
 }
 
-#[cfg(feature="load_progress")]
+#[cfg(feature = "load_progress")]
 pub(crate) fn check_assets_ready(
     world: Query<(&GameWorldTag, &BlueprintInstanceReady)>,
     progress: ProgressEntry<AssetLoadState>,
@@ -112,7 +112,7 @@ pub(crate) fn check_assets_ready(
 ) {
     if world.get_single().is_ok() {
         info!("checking assets");
-        
+
         if !*initted {
             progress.set_total(
                 image_assets_loading.0.len() as u32 + mesh_assets_loading.0.len() as u32,
@@ -136,7 +136,8 @@ pub(crate) fn check_assets_ready(
             }
         }
         info!("checking meshes");
-    }}
+    }
+}
 //         for mesh_and_scene in &mesh_assets_loading.0 {
 //             match server.get_load_state(&mesh_and_scene.clone()).unwrap() {
 //                 bevy::asset::LoadState::Failed(err) => {
