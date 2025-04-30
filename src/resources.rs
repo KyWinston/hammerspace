5448375b679928e928542d8c19c3118050146c27
KyleWinston <kyle.w.r@hotmail.com>
2025-04-30 15:17:22 -0400

    remove deprecated

--- src/resources.rs
+++ src/resources.rs
@@ -1,20 +1,14 @@
 use bevy::prelude::*;
 
-#[derive(Resource,Clone)]
+#[derive(Resource, Clone)]
 pub struct HammerspaceConfig {
     pub level_folder: String,
-    pub lights_identifier: String,
-    pub collision_identifier: String,
-    pub spawn_identifier: String,
 }
 
 impl Default for HammerspaceConfig {
     fn default() -> Self {
         Self {
             level_folder: "".to_string(),
-            lights_identifier: "_light".to_string(),
-            collision_identifier: "_colllider".to_string(),
-            spawn_identifier: "_spawn".to_string(),
         }
     }
 }
